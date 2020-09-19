fe1e8f511a25800022843c5406173ee78ebd0599
Ragereous <ragereous@gmail.com>
2020-09-19 17:44:29 -0400

    project completed, tests green

--- CarGarage/Car.cs
+++ CarGarage/Car.cs
@@ -11,6 +11,57 @@ namespace CarGarage
         public int Speed { get; set; }
         public int FuelLevel { get; set; }
         public bool AutoBrakeActive { get; set; }
-
+        public void SetMake(string make)
+        {
+            make = "Honda";
+            Make = make;
+        }
+        public void SetModel(string model)
+        {
+            model = "Accord";
+            Model = model;
+        }
+        public int GetSpeed()
+        {
+            return Speed;
+        }
+        public bool IsSpeeding()
+        {
+            if (Speed >= 71)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Accelerate()
+        {
+            if (Make == "Ferrari" && Speed <= 80)
+            {
+                Speed += 20;
+            }
+            else if (Speed <= 90)
+            {
+                Speed += 10;
+            }
+        }
+        public void Brake()
+        {
+            if (AutoBrakeActive == true)
+            {
+                Speed = Speed /= 2;
+            }
+            else if (Speed >= 7)
+            {
+                Speed -= 7;
+            }
+            else if (Speed < 7 && Speed >= 0)
+            {
+                Speed--;
+            }
+        }
     }
+
 }
